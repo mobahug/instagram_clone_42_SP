9bda008c40754e0d8eae30b52a27768cc19523c6
Gabor Horvath-Ulenius <mobahug@gmail.com>
2022-06-28 15:12:53 +0300

    added fixes to see better on phone liker page

--- likers.php
+++ likers.php
@@ -7,6 +7,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>mangofruit.fi</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
